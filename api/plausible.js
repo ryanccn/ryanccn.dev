0ef7191c64be252034816320759062ac2e225c66
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2021-09-25 16:29:40 +0800

    use totally random string

--- api/plausible.js
+++ api/plausible.js
@@ -2,11 +2,27 @@ import got from 'got';
 
 const originalUrl = 'https://plausible.io/js/plausible.js';
 
+const randomStr = () => {
+  const chars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  let str = '';
+  for (let i = 0; i < 20; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return str;
+};
+
 module.exports = async (_, res) => {
+  const replacement = randomStr();
   const origin = await got(originalUrl);
+
   const script = origin.body
-    .replace(/window.plausible/g, 'window.syCCxbd3Ch')
+    .replace(/window.plausible/g, `window.${replacement}`)
     .replace(/plausible_ignore/g, 'plausibleIgnore');
+
+  console.log('plausible replacement:', replacement);
 
   res.setHeader('content-type', 'text/javascript');
   res.setHeader('cache-control', 's-maxage=3600');
