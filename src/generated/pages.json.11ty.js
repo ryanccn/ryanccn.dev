0a50fcafd507ca0c08dde20670e7eba976f6590c
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-04-06 18:31:01 +0800

    chore: format pages.json for easier reading

--- src/generated/pages.json.11ty.js
+++ src/generated/pages.json.11ty.js
@@ -8,21 +8,25 @@ class Page {
   }
 
   render(data) {
-    return JSON.stringify([
-      ...data.collections.all.map((page) => {
-        // const titleFragments = page.data.title.split(' ');
-        // const titleWithNbsp =
-        //   titleFragments.slice(0, titleFragments.length - 1).join(' ') +
-        //   ' ' +
-        //   titleFragments[titleFragments.length - 1];
+    return JSON.stringify(
+      [
+        ...data.collections.all.map((page) => {
+          // const titleFragments = page.data.title.split(' ');
+          // const titleWithNbsp =
+          //   titleFragments.slice(0, titleFragments.length - 1).join(' ') +
+          //   ' ' +
+          //   titleFragments[titleFragments.length - 1];
 
-        return {
-          title: page.data.title,
-          slug: this.slugify(page.data.title),
-          date: page.data.date,
-        };
-      }),
-    ]);
+          return {
+            title: page.data.title,
+            slug: this.slugify(page.data.title),
+            date: page.data.date,
+          };
+        }),
+      ],
+      undefined,
+      2
+    );
   }
 }
 
