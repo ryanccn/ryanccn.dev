1bf48188a6ba6e704c30a2764fe1b58c1ee04b7e
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-02 21:54:15 +0800

    chore: update projects

--- src/_data/gitRev.js
+++ src/_data/gitRev.js
@@ -1,6 +1,4 @@
 module.exports = async () => {
   const { execa } = await import('execa');
-  return await execa('git', ['rev-parse', '--short', 'HEAD']).then(
-    (a) => a.stdout,
-  );
+  return execa('git', ['rev-parse', '--short', 'HEAD']).then((a) => a.stdout);
 };
