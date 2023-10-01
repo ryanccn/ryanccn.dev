7f6d147dee0b2dfe087faedae74c2c282f19613f
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-01 13:01:25 +0800

    feat: limit feeds to 10 items

--- src/generated/feed.json.11ty.js
+++ src/generated/feed.json.11ty.js
@@ -14,17 +14,20 @@ class Page {
         home_page_url: 'https://ryanccn.dev/',
         feed_url: 'https://ryanccn.dev/feed/feed.json',
         items: await Promise.all(
-          [...data.collections.posts].reverse().map(async (post) => ({
-            id: post.url,
-            url: this.absoluteUrl(post.url),
-            title: post.data.title,
-            content_html: await this.htmlToAbsoluteUrls(
-              post.templateContent,
-              this.absoluteUrl(post.url),
-            ),
-            date_published: post.data.date.toISOString(),
-            tags: post.data.tags,
-          })),
+          [...data.collections.posts]
+            .reverse()
+            .slice(0, 5)
+            .map(async (post) => ({
+              id: post.url,
+              url: this.absoluteUrl(post.url),
+              title: post.data.title,
+              content_html: await this.htmlToAbsoluteUrls(
+                post.templateContent,
+                this.absoluteUrl(post.url),
+              ),
+              date_published: post.data.date.toISOString(),
+              tags: post.data.tags,
+            })),
         ),
       },
       undefined,
