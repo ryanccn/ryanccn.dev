316749c6eb1015be96968196e3702b16e4dc3116
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-16 19:50:44 +0800

    chore: bump deps

--- src/_11ty/shortcodes/warning.js
+++ src/_11ty/shortcodes/warning.js
@@ -3,7 +3,7 @@ const { html } = require('../../utils/htmlTag');
 module.exports = (content) => {
   return html`
     <div
-      class="not-prose flex flex-col gap-y-1 border-l-2 border-tailwind-yellow-400 dark:border-tailwind-yellow-600 px-6 py-2 !mb-10"
+      class="not-prose !mb-10 flex flex-col gap-y-1 border-l-2 border-tailwind-yellow-400 px-6 py-2 dark:border-tailwind-yellow-600"
     >
       <p
         class="text-lg font-bold text-tailwind-yellow-400 dark:text-tailwind-yellow-500"
